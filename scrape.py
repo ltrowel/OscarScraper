642e9065548fa726f62aa2eb6b7b6f9ca10a00d5
Liam Trowel <trowel.liam@gmail.com>
2020-01-19 20:50:03 -0500

    Fix edge case for ceremonies ending in teens

--- scrape.py
+++ scrape.py
@@ -8,14 +8,18 @@ def get_ceremony_from_year(year):
     # First Oscar's was in 1929, but there were two ceremonies in 1930
     # TODO: Figure out how to scrape the 2nd and 3rd ceremonies better
     ceremony_number = year - 1928
-    # The modulo of 10 will return the last digit of any number
-    ceremony_last_digit = ceremony_number % 10
+
+    # Find the ordinal indicator based on the last digits of the number
+    # Anything that is a 'teen' breaks the usual rules of 1st, 2nd, 3rd
+    last_digit = ceremony_number % 10
+    second_to_last_digit = int((ceremony_number/10)) % 10
+
     ordinal_indicator = 'th'
-    if ceremony_last_digit == 1:
+    if last_digit == 1 and second_to_last_digit != 1:
         ordinal_indicator = 'st'
-    elif ceremony_last_digit == 2:
+    elif last_digit == 2 and second_to_last_digit != 1:
         ordinal_indicator = 'nd'
-    elif ceremony_last_digit == 3:
+    elif last_digit == 3 and second_to_last_digit != 1:
         ordinal_indicator = 'rd'
 
     return '{number}{ordinal_indicator} Academy Awards'.format(
